50d3f4fcd189f1eb60773d4290bcc7b20f6c5fc4
Markus Johansson <markus@enkelmedia.se>
2025-11-15 21:07:12 +0100

    v17 initial upgrade for rc

--- src/NewsletterStudio.Plugins.UmbracoForms/Client/openapi-ts.config.ts
+++ src/NewsletterStudio.Plugins.UmbracoForms/Client/openapi-ts.config.ts
@@ -4,27 +4,29 @@ export default defineConfig({
 	input: 'http://localhost:54108/umbraco/swagger/newsletter-studio-plugin/swagger.json',
 	output: {
 		path: './src/backend-api',
-    format:'prettier',
-    lint:'eslint'
+    format: 'prettier',
+    lint: 'eslint'
 	},
-
 	plugins: [
     ...defaultPlugins,
 		{
 			name: '@hey-api/client-fetch',
-			bundle: false,
+
+      // configure to re-export from index.ts
 			exportFromIndex: true,
+
+      // setting to throw since we want to trigger Umbraco's error handling to get a UmbApiError
+      // with problemDetails back from tryExecute.
 			throwOnError: true,
 		},
 		{
 			name: '@hey-api/typescript',
 			enums: 'typescript',
-      readOnlyWriteOnlyBehavior : 'off'
 		},
 		{
 			name: '@hey-api/sdk',
 			asClass: true,
-      serviceNameBuilder : '{{name}}Resource',
+      classNameBuilder : '{{name}}Resource'
 		}
 	]
 });
