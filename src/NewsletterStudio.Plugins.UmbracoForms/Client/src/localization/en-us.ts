9df440ad26bb82457779f5d8722dbe3b92297b8d
Markus Johansson <markus@enkelmedia.se>
2025-11-26 22:37:11 +0100

    Localization: Updates to Transactional Email Workflow descriptions

--- src/NewsletterStudio.Plugins.UmbracoForms/Client/src/localization/en-us.ts
+++ src/NewsletterStudio.Plugins.UmbracoForms/Client/src/localization/en-us.ts
@@ -3,10 +3,10 @@ import type { UmbLocalizationDictionary } from '@umbraco-cms/backoffice/localiza
 export default {
   formProviderWorkflows : {
     TransactionalEmailLabel : 'Transactional Email',
-    TransactionalEmailDescription : 'Pick the Transactional Email to send',
+    TransactionalEmailDescription : 'Pick the Transactional Email Template to send',
 
     SendToEmailLabel : 'Send to Email',
-    SendToEmailDescription : 'Optional. Use this to send to a email from a form field.',
+    SendToEmailDescription : 'Optional. Sends the email to this address in addition to the recipients configured in the Transactional Email Template.',
 
     AddToMailingListConfigurationLabel : 'Mailing List Configuration',
     AddToMailingListConfigurationDescription : 'Choose a mailing list and mapp the fields.'
