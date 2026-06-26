ba304756a45a01e53d3346f003ceabb58ee40daa
Markus Johansson <markus@enkelmedia.se>
2026-06-27 01:27:46 +0200

    Bump Umbraco-dependency to 18.0.0

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,4 @@
-param ($version = "17.3.0", $versionSuffix = "")
+param ($version = "18.0.0", $versionSuffix = "")
 # Params
 # version = major.minor.patch
 # versionSuffix = eg -beta1, -rc1 (should include leading -). Leave as empty string if not needed.
